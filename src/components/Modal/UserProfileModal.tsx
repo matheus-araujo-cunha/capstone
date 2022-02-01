4cdbe768fdceeeb67eb39c68bf2beb96325b3a0e
Pablo Adriano <pabloadrianomc@gmail.com>
2022-01-31 21:25:01 -0300

    feat: style the user profile card modal
    Add a avatar with user first letter.
    Show user location.
    Add a "about me" section.

--- src/components/Modal/UserProfileModal.tsx
+++ src/components/Modal/UserProfileModal.tsx
@@ -1,12 +1,23 @@
+import { LocationOnOutlined } from "@mui/icons-material";
 import {
+  Avatar,
   Button,
+  Box,
+  Card,
+  CardActions,
+  CardContent,
+  Divider,
+  CardHeader,
+  CardMedia,
   Dialog,
   DialogActions,
   DialogContent,
   DialogContentText,
   DialogTitle,
+  Typography as Text,
 } from "@mui/material";
 
+import { blue } from "@mui/material/colors";
 interface UserProfileModalProps {
   handleCloseProfileModal: () => void;
   openProfileModal: boolean;
@@ -19,15 +30,58 @@ const UserProfileModal = ({
   return (
     <>
       <Dialog open={openProfileModal} onClose={handleCloseProfileModal}>
-        <DialogTitle>Meu perfil</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            Faça as alterações que achar necessário e depois clique em salvar.
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleCloseProfileModal}>Fechar</Button>
-        </DialogActions>
+        <Card sx={{ width: "auto", height: 300 }}>
+          <Box bgcolor="primary.main" sx={{ p: 2, pr: 6 }}>
+            <Text gutterBottom variant="h5" component="h2">
+              Kenzinho
+            </Text>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "flex-end",
+              }}
+            >
+              <Avatar
+                sx={{
+                  border: 2,
+                  borderColor: "#fff",
+                  bgcolor: blue[500],
+                  width: 64,
+                  height: 64,
+                  fontSize: 30,
+                }}
+                aria-label="Kenzinho"
+              >
+                K
+              </Avatar>
+              <Box>
+                <Text variant="body2" color="#fff" fontWeight="bold">
+                  <LocationOnOutlined />
+                  Curitiba, PR
+                </Text>
+              </Box>
+            </Box>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-between",
+              height: "55%",
+            }}
+          >
+            <CardContent>
+              <Text fontWeight="bold">SOBRE MIM</Text>
+              <Text>Possuo vários veículos disponíveis para alugar.</Text>
+            </CardContent>
+            <CardActions>
+              <Button size="small" onClick={handleCloseProfileModal}>
+                Voltar
+              </Button>
+            </CardActions>
+          </Box>
+        </Card>
       </Dialog>
     </>
   );
